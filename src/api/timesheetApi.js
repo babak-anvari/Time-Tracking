23c49b7ed05b1631cc02f441a032de4128e55621
Babak <babak.anvari@gmail.com>
2020-11-21 11:45:09 -0800

    feat (api call) configure webpack dev environment

--- src/api/timesheetApi.js
+++ src/api/timesheetApi.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+const baseUrl = process.env.API_URL + '/timesheet'
+
+
 export const getTimesheet = async () => {
-    let response = await axios({
+    return (await axios({
         method: 'get',
-        url: 'http://localhost:7777/timesheet',
+        url: baseUrl,
         //add header here later
         //add params here later
-    })
-    return response.data;
+    })).data;
 }
