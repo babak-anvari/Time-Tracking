61ac788bfab67848815ba7da047592e112fb4a11
Babak <babak.anvari@gmail.com>
2020-11-25 17:42:55 -0800

    feat (api call): handle api response and error

--- src/api/timesheetApi.js
+++ src/api/timesheetApi.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 import { handleResponse, handleError } from './apiUtils'
 
-const baseUrl = process.env.API_URL + '/timesheet'
+const baseUrl = process.env.API_URL + '/timesheet/'
 
 export const getTimesheet = () => {
     return axios({
         method: 'get',
         url: baseUrl,
+        params: {
+            weekEnd: '2020-09-30',
+            userId: '5f5c080d01b43d9958fe2d54'
+        }
     })
         .then(handleResponse)
         .catch(handleError)
