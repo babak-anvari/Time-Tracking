f40abec3eed24ba32e4322bde0086cb8dd48dae6
Babak <babak.anvari@gmail.com>
2021-01-15 19:25:46 -0800

    feat(UI): improve navigation UI

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -3,6 +3,6 @@ import React from 'react';
 export const AboutPage = () => (
     <div className='jumbotron'>
         <h2>About</h2>
-        <p>This application is developed by Babak to track time spent on projects.</p>
+        <p>Time tracking web application</p>
     </div>
 );
