7ca85090dc9f71b6d634cfe12439f0ccc17a6d43
Babak <babak.anvari@gmail.com>
2021-03-31 22:35:44 -0700

    fix(server): deploy environement variables

--- api/server.js
+++ api/server.js
@@ -17,5 +17,8 @@ if (config.env == config.prod) {
     app.get('/*', (req, res) => {
         res.sendFile(path.resolve('index.html'));
     })
-    console.log(process.env.NODE_ENV);
+    console.log(`Environment: ${process.env.NODE_ENV}`);
+    console.log(`URL: ${process.env.API_URL}`);
+    console.log(`Port: ${process.env.PORT}`);
+    console.log(`MongoDB URL: ${process.env.MONGODB_URI}`);
 }
