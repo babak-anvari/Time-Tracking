43ed26d02edcabfa1858eacb6ba3c711e3bbbd54
Babak <babak.anvari@gmail.com>
2021-04-07 22:57:30 -0700

    refactor(app): use bootstrap components

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -2,25 +2,42 @@ import React from 'react';
 import PropTypes from "prop-types";
 import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded';
 import RemoveCircleOutlineRoundedIcon from '@material-ui/icons/RemoveCircleOutlineRounded';
+import { InputGroup, FormControl, Table } from "react-bootstrap";
 
 const ProjectForm = ({ identifiedProject, saveIdentifiedProject, setProjectComponentState, actionList, handleProjectChange, updateProjectActions, actionAssigned }) => (
     <>
-        <h3>Project Information</h3>
+        <h4>Project Information</h4>
+        <hr></hr>
         <form>
-            <div>
-                <label>Project Name</label>
-                <input onChange={handleProjectChange} name={'number'} value={identifiedProject.number} placeholder='Name' />
+            <div className="margin-top-20">
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="firstName" className="width-150">Project Name</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        onChange={handleProjectChange}
+                        name={'number'}
+                        value={identifiedProject.number}
+                        placeholder='Name'
+                    />
+                </InputGroup>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="firstName" className="width-150">Project Address</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        onChange={handleProjectChange}
+                        name={'address'}
+                        value={identifiedProject.address}
+                        placeholder='address'
+                    />
+                </InputGroup>
             </div>
-            <div>
-                <label>Project Address</label>
-                <input onChange={handleProjectChange} name={'address'} value={identifiedProject.address} placeholder='address' />
-            </div>
-
-            <table>
+            <Table striped bordered hover size="sm">
                 <thead>
                     <tr>
                         <th>Actions</th>
-                        <th>Add/ Remove actions</th>
+                        <th>Add / Remove actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -40,7 +57,7 @@ const ProjectForm = ({ identifiedProject, saveIdentifiedProject, setProjectCompo
                         </tr>
                     ))}
                 </tbody>
-            </table ><br />
+            </Table>
         </form>
         <button onClick={saveIdentifiedProject} className='btn btn-primary btn-sm'>Save</button><br /><br />
         <button onClick={() => { setProjectComponentState('search') }} className='btn btn-primary btn-sm'>Back to projects</button><br /><br />
